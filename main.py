0e1af3c0517e5264c592accd69ac92b543d996d6
rjm3q <robert.mabry86@gmail.com>
2019-06-22 12:36:05 -0500

    intial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 18:26:05 2019
+
+@author: rober
+"""
+
+import os
+import csv
+currentDir = os.getcwd()
+path = os.path.join(currentDir, 'Resource', 'budget_data.csv')
+months = []
+avglist = []
+avgchange = []
+with open(path, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader)
+    for row in csvreader:
+        months.append(row[0])
+        avglist.append(int(row[1]))
+#average of changes in profit/loss
+    total = sum(avglist)
+#greatest increase in profit
+    for loss in avglist:
+        if loss == avglist[0]:
+            last = loss
+        else:
+            new = loss - last
+            avgchange.append(new)
+            last = loss
+
+    avg = sum(avgchange) / (len(months)-1)
+#greatest decrease in loss
+    incr = avgchange.index(max(avgchange))
+
+
+
+
+
+print ("Financial Analysis")
+print("-------------------------------")
