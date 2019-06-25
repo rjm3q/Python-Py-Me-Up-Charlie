290342a99b75c160a042cb2d55a64673dede87d1
rjm3q <robert.mabry86@gmail.com>
2019-06-25 17:58:38 -0500

    new2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Created on Thu Jun 13 18:26:05 2019
 import os
 import csv
 currentDir = os.getcwd()
-path = os.path.join(currentDir, 'Resource', 'budget_data.csv')
+path = os.path.join('budget_data.csv')
 months = []
 avglist = []
 avgchange = []
@@ -32,10 +32,15 @@ with open(path, newline='') as csvfile:
     avg = sum(avgchange) / (len(months)-1)
 #greatest decrease in loss
     incr = avgchange.index(max(avgchange))
-
+    decr = avgchange.index(min(avgchange))
 
 
 
 
 print ("Financial Analysis")
 print("-------------------------------")
+print("Total Months: " + str(len(months)))
+print("Total: $" + str(total))
+print("Averagre Change: " + str(avg))
+print("Greatest Increase: " + str(incr))
+print("Greatest Decreas: " + str(decr))
