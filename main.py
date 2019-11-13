881013ed57622a952e4192ebb8b8a896c4886baf
rjm3q <robert.mabry86@gmail.com>
2019-11-12 21:27:35 -0600

    update for final

--- main.py
+++ main.py
@@ -7,35 +7,62 @@ Created on Thu Jun 13 18:26:05 2019
 
 import os
 import csv
-currentDir = os.getcwd()
-path = os.path.join(currentDir, 'Resource', 'budget_data.csv')
-months = []
-avglist = []
-avgchange = []
-with open(path, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csvheader = next(csvreader)
+
+#setfilepath
+csvpath= os.path.join("budget_data.csv")
+
+#lists
+months= []
+profits_losses= []
+changes= []
+
+#opens the csv file to read
+with open(csvpath, newline='', encoding='utf8') as budget_data:
+    csvreader = csv.reader(budget_data, delimiter=",")
+    csv_header = next(csvreader)
+
+    #populate lists
     for row in csvreader:
         months.append(row[0])
-        avglist.append(int(row[1]))
-#average of changes in profit/loss
-    total = sum(avglist)
-#greatest increase in profit
-    for loss in avglist:
-        if loss == avglist[0]:
-            last = loss
+        profits_losses.append(int(row[1]))
+
+    total = sum(profits_losses)
+
+    #calculates change by month
+    for prof_loss in profits_losses:
+        if prof_loss == profits_losses[0]:
+            previous= prof_loss
         else:
-            new = loss - last
-            avgchange.append(new)
-            last = loss
+            change= prof_loss - previous
+            changes.append(change)
+            previous= prof_loss
 
-    avg = sum(avgchange) / (len(months)-1)
-#greatest decrease in loss
-    incr = avgchange.index(max(avgchange))
+    #assigns variables for printing
+    average= sum(changes) / (len(months)-1)
+    greatest_increase_index= changes.index(max(changes))
+    greatest_decrease_index= changes.index(min(changes))
 
+#output
+print(f"""
+-----------------
+Financial Data
+-----------------
+Total Number of Months: {len(months)}
+Total Profits/Losses: ${total}
+Average Change: ${round(average,2)}
+Greatest Increase: {months[greatest_increase_index + 1]} (${changes[greatest_increase_index]})
+Greatest Decrease: {months[greatest_decrease_index +1]} (${changes[greatest_decrease_index]})
+""")
 
+output_file= os.path.join("PyBank_output.txt")
 
-
-
-print ("Financial Analysis")
-print("-------------------------------")
+output= open(output_file, "w")
+output.write(f"""
+-----------------
+Financial Data
+-----------------
+Total Number of Months: {len(months)}
+Total Profits/Losses: ${total}
+Average Change: ${round(average,2)}
+Greatest Increase: {months[greatest_increase_index + 1]} (${changes[greatest_increase_index]})
+Greatest Decrease: {months[greatest_decrease_index +1]} (${changes[greatest_decrease_index]})
